97f67dbd05076dea574fce15b0e9e591ae652bab
anulalat <ndra.bodoh@gmail.com>
2024-02-13 07:55:51 +0700

    Revert "Native: ProducerListener: Import ON_BUFFER_DETACHED Changes"
    This reverts commit fe3c5bb5234cb490adadfe0c5ec19f3324aa04b1.

--- libs/gui/include/gui/Surface.h
+++ libs/gui/include/gui/Surface.h
@@ -56,11 +56,6 @@ public:
     virtual bool needsReleaseNotify() = 0;
 
     virtual void onBuffersDiscarded(const std::vector<sp<GraphicBuffer>>& buffers) = 0;
-
-    virtual void onBufferDetached(int /**slot**/) {
-        //default do nothing
-    }
-
 };
 
 /*
@@ -417,11 +412,6 @@ protected:
         }
 
         virtual void onBuffersDiscarded(const std::vector<int32_t>& slots);
-
-        virtual void onBufferDetached(int slot) {
-            mSurfaceListener->onBufferDetached(slot);
-        }
-
     private:
         wp<Surface> mParent;
         sp<SurfaceListener> mSurfaceListener;
